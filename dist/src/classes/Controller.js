0e088ab43d4a74359a04e1011a43b0ee3d2b0b7a
RuMont <rumont98@gmail.com>
2022-10-06 23:18:21 +0200

    tsc dep

--- dist/src/classes/Controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Controller = void 0;
-class Controller {
-    static index(req, res) { }
-}
-exports.Controller = Controller;
